a1c368f4b0b24847ddeef41b8606b06edafb2c11
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-04-08 21:37:33 -0400

    Subscription events added

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,20 +1,54 @@
 import userModel from "../../models/user.model";
 import subsModel from "../../models/subsucription.model";
+import { connectDB } from "../../database/db";
 
 const handler = async (req, res) => {
+  await connectDB();
   const body = req.body;
+  console.log(body);
   if (req.method === "POST") {
-    console.log({ body });
+
+    //Check if exist a event type
     if (body.event_type) {
+
+      //check if exist a subscription pending or other
+      const sub = await subsModel.findOne({ paypalId: body.resource.id });
+      if (!sub) return res.status(400).json({ error: "sub does't exist" });
+
+      //Check the king of the event
       switch (body.event_type) {
+
+        //if a subscription is activated
         case "BILLING.SUBSCRIPTION.ACTIVATED":
-          const sub = await subsModel.findOne({ paypalId: body.resource.id });
-          if (!sub) return res.status(400).json({ error: "sub does't exist" });
-          const updateSub = await subsModel.findByIdAndUpdate(sub._id, {
+          await subsModel.findByIdAndUpdate(sub._id, {
             status: body.resource.status,
           });
           res.status(200).json({ message: "Done" });
           break;
+
+        //if a subsciption pyment is failed  
+        case "BILLING.SUBSCRIPTION.PAYMENT.FAILED":
+          await subsModel.findByIdAndUpdate(sub._id, {
+            status: "PAYMENT_FAILED",
+          });
+          res.status(200).json({ message: "Done" });
+          break;
+
+        //if a subscription is cacelled
+        case "BILLING.SUBSCRIPTION.CANCELLED":
+          await subsModel.findByIdAndUpdate(sub._id, {
+            status: body.resource.status,
+          });
+          res.status(200).json({ message: "Done" });
+          break;
+        
+        case "BILLING.SUBSCRIPTION.SUSPENDED":
+          await subsModel.findByIdAndUpdate(sub._id, {
+            status: body.resource.status,
+          });
+          res.status(200).json({message: "Done"})
+          break;
+
       }
     }
   } else {
