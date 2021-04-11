f6024a11f32349a4975c933cc95c97f362767000
genarogg <genarrogg@gmail.com>
2021-04-11 11:10:54 -0400

    dashboard

---
+++ components/views/home/HomeDashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Precio from "./Precio"
+
+
+const HomeDashboard = () => {
+  
+  return (
+    <>
+      <div className="content row col-xs-12" id="content">
+       <Precio/>
+      </div>
+    </>
+  );
+};
+
+export default HomeDashboard;
