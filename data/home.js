e444607d4c73a9383c24fb0256e9675c8b8621f6
genarogg <genarrogg@gmail.com>
2021-04-13 16:35:04 -0400

    data

---
+++ data/home.js
@@ -0,0 +1,14 @@
+const data = () => {
+  const data = {
+    bienvenida: "Bienvenido a TagBlade",
+    subBienvenida: "¡Busca, maneja y analiza Instagram hashtags con TagBlade!",
+    /* esta descripcion debe de contar con una longitud entre 120 a 150 no mas */
+    descripcion: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+  quis, in dolor dignissimos fuga, numquam eveniet ut corrupti
+  ipsa possimus molestiae quo eligendi quia officia aperiam ullam
+  repellat incidunt distinctio.`,
+  };
+  return data;
+};
+
+export default data;
---
+++ data/precios.js
@@ -0,0 +1,16 @@
+const data = () => {
+  const data = {
+    titleR1: "básico",
+    contenidoR1:
+      "For teams learning MongoDB or developing small applications. Get 512MB storage free and scale up to 5GB.",
+    titleR2: "Community Manager",
+    contenidoR2:
+      "For teams building applications that need advanced development or production-ready environments.",
+    titleR3: "Agencia",
+    contenidoR3:
+      "For teams developing world-class applications that require multi-region resiliency or ultra-low latency access.",
+  };
+  return data;
+};
+
+export default data;
