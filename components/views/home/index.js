793bcc829f7dc3174ae499e9abe7c7e29b3e50fa
genarogg <genarrogg@gmail.com>
2021-04-13 16:37:08 -0400

    vistas de precios

--- components/views/home/index.js
+++ components/views/home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import HomeDashboard from "./HomeDashboard"
+
 import Register from "../../forms/Register";
 import Login from "../../forms/Login";
 import ResetPassword from "../../forms/ResetPassword";
+import HomeDashboard from "./HomeDashboard";
 
-const HomeLogin = () => {
+import data from "../../../data/home";
+
+const Home = () => {
   return (
     <>
       <main>
@@ -12,15 +15,10 @@ const HomeLogin = () => {
         <div className="backgroundLigth"></div>
         <div className="containerSesion row between-md center-xs">
           <div className="welcome row col-xs-12 col-sm-6  col-lg-4 center-xs">
-            <h1>Bienvenido a TagBlade </h1>
-            <h3>¡Busca, maneja y analiza Instagram hashtags con TagBlade!</h3>
+            <h1>{data().bienvenida}</h1>
+            <h3>{data().subBienvenida}</h3>
             <div className="text">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
-                quis, in dolor dignissimos fuga, numquam eveniet ut corrupti
-                ipsa possimus molestiae quo eligendi quia officia aperiam ullam
-                repellat incidunt distinctio.
-              </p>
+              <p>{data().descripcion}</p>
             </div>
           </div>
           <div className="containerForm  " id="containerRegisterLogin">
@@ -35,4 +33,4 @@ const HomeLogin = () => {
   );
 };
 
-export default HomeLogin;
+export default Home;
