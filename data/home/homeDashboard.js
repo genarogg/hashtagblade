c2f7e0db1f2d381877d73b74813baae439f0e792
genarogg <genarrogg@gmail.com>
2021-04-28 04:16:26 -0400

    dashboard

---
+++ data/home/homeDashboard.js
@@ -0,0 +1,27 @@
+const data = () => {
+  const data = {
+    card1: {
+      titulo: "Hashtag Search",
+      descripcion:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores deserunt voluptas minima ut, quaerat aspernatur voluptatem esse quis eligendi.",
+    },
+    card2: {
+      titulo: "Hashtag Collections",
+      descripcion:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores deserunt voluptas minima ut, quaerat aspernatur voluptatem esse quis eligendi.",
+    },
+    card3: {
+      titulo: "Store & Manage hashtags",
+      descripcion:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores deserunt voluptas minima ut, quaerat aspernatur voluptatem esse quis eligendi.",
+    },
+    card4: {
+      titulo: "Average Best Rank",
+      descripcion:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores deserunt voluptas minima ut, quaerat aspernatur voluptatem esse quis eligendi.",
+    },
+  };
+  return data;
+};
+
+export default data;
