1ba0a3a4605f37b7c105c05d39ccc2d3d1163666
genarogg <genarrogg@gmail.com>
2021-05-02 18:30:03 -0400

    CardAnadirColeccion

---
+++ components/general/dashboard/card/CardAnadirColeccion.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardAnadirColeccion = () => {
+  return (
+    <div className="row estadistica new col-sm-4 col-xs-12">
+      <span>+</span>
+    </div>
+  );
+};
+
+export default CardAnadirColeccion;
