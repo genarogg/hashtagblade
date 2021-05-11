24c395e5e8476826da19c384e0f460ccda326a25
genarogg <genarrogg@gmail.com>
2021-05-11 17:12:37 -0400

    nuevos modulos npm

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ import "../styles/response/index.css";
 import "../styles/loading_screen.css";
 
 import "../styles/dashboard/index.css"
-import "../styles/colorChange/index.css"
+/* import "../styles/colorChange/index.css" */
 
 import data from "../data/home/home";
 
