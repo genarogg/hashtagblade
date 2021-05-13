158e3d60b5f0c8c8daab8944bb96fa8a4bb95adc
genarogg <genarrogg@gmail.com>
2021-05-12 22:28:03 -0400

    fecth

--- components/general/dashboard/SearchHashtag.js
+++ components/general/dashboard/SearchHashtag.js
@@ -5,53 +5,52 @@ import data from "../../../data/dashboard/fakeDataTAG";
 import $ from "../../nano/$";
 
 const SearchHashtag = () => {
-  const datos = data()
-  
+  const datos = data();
+
   const filter = async () => {
-    let filtrado
+    let filtrado;
     let texto = $("inputTagFilter").value.toLowerCase();
     let arregloNew = [];
-    let contNew = 0
-    const length1 = datos.length
+    let contNew = 0;
+    const length1 = datos.length;
 
-    for(let i = 0 ; i< length1 ; i++){
-      let nombre = datos[i].toLowerCase()
-      filtrado = nombre.indexOf(texto) !== -1
+    for (let i = 0; i < length1; i++) {
+      let nombre = datos[i].toLowerCase();
+      filtrado = nombre.indexOf(texto) !== -1;
 
-      if(filtrado){
+      if (filtrado) {
         arregloNew[contNew++] = nombre;
       }
     }
-  
+
     console.log("Respuestas");
     console.log(filtrado);
     console.log(arregloNew);
 
-    
-
-    if(true){
-      $("filterUl").innerHTML = arregloNew.map(e =>{
-        return `<li>${e}</li>`
-      })
+    if (true) {
+      $("filterUl").innerHTML = arregloNew.map((e) => {
+        return `<li>${e}</li>`;
+      });
     }
 
-    const hola2 = fetch("/api/hashtag/test",{
+    const hola2 = fetch("/api/hashtag/test", {
       method: "POST",
       headers: {
-        "Content-Type":"application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        hashtags:["hola","adios","love","tag","json"]
-      })
-    }).then(data=>{})
+        hashtags: ["hola", "adios", "love", "tag", "json"],
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
 
-    console.log(hola2)
+    console.log(hola2);
   };
 
   return (
     <>
       <div className="searchHashtag">
-        
         <input
           type="text"
           id="inputTagFilter"
@@ -63,9 +62,7 @@ const SearchHashtag = () => {
         <label htmlFor="">
           <Icono css="icon-search" />
         </label>
-        <ul className="filterUl" id="filterUl">
-
-        </ul>
+        <ul className="filterUl" id="filterUl"></ul>
       </div>
     </>
   );
